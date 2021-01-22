a156e5f50915de97da09b97bb4decbc15dbf31f4
Suman Maharjan <suman@digitalflowstudio.com>
2021-01-22 13:38:11 +0545

    daterangepicker issue

--- wp-content/themes/juanderer/partials/flexible-content/enquiry_form.php
+++ wp-content/themes/juanderer/partials/flexible-content/enquiry_form.php
@@ -57,24 +57,7 @@ if (($_GET['tour_id'])) {
 <script>
     jQuery('[name="tour"]').val('<?= $tour_name; ?>');
 
-    <!-- DATEPICKER  -->
-    $(function () {
-        $('input[name="dates"]').daterangepicker({
-            autoUpdateInput: false,
-            parentEl: '.scroll-fix',
-            minDate: new Date(),
-            opens: 'left',
-            locale: {
-                cancelLabel: 'Clear'
-            }
-        });
-        $('input[name="dates"]').on('apply.daterangepicker', function (ev, picker) {
-            $(this).val(picker.startDate.format('MM-DD-YY') + ' > ' + picker.endDate.format('MM-DD-YY'));
-        });
-        $('input[name="dates"]').on('cancel.daterangepicker', function (ev, picker) {
-            $(this).val('');
-        });
-    });
+
 
 </script>
 
