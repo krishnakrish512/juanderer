1ad755ae84f083292e7a6072f8c334521d1e02bc
Suman Maharjan <suman@digitalflowstudio.com>
2021-02-04 17:50:14 +0545

    changes footer

--- wp-content/themes/juanderer/footer.php
+++ wp-content/themes/juanderer/footer.php
@@ -2,7 +2,7 @@
     <?php $contact = get_field('contact', 'option'); ?>
     <?php $social = get_field('social', 'option'); ?>
     <div class="container margin_60_35">
-        <div class="row">
+        <div class="row justify-content-between">
             <div class="col-lg-5 col-md-12 p-r-5">
                 <p><img src="<?= $contact['footer_logo'] ?>" height="70" alt=""></p>
                 <p><?= $contact['description'] ?></p>
@@ -27,26 +27,32 @@
                     </ul>
                 </div>
             </div>
-            <div class="col-lg-3 col-md-6">
-                <h5>Thailand Address:</h5>
-                <ul class="contacts">
-                    <li><?= $contact['thailand_address'] ?></li>
-                    <li><a href="tel://+6696 704 6245"><i class="ti-mobile"></i> <?= $contact['thailand_number'] ?></a>
-                    </li>
-                    <li><a href="mailto:info@juanderertravels.com"><i class="ti-email"></i> <?= $contact['email'] ?></a>
-                    </li>
-                </ul>
+            <div class="col-lg-6 col-md-12">
+                <h5>Contact Address</h5>
+                <div class="row">
+                    <div class="col-lg-12 col-md-6">
+                        <h5>Thailand Address:</h5>
+                        <ul class="contacts">
+                            <li><?= $contact['thailand_address'] ?></li>
+                            <li><a href="tel://+6696 704 6245"><i class="ti-mobile"></i> <?= $contact['thailand_number'] ?></a>
+                            </li>
+                            <li><a href="mailto:info@juanderertravels.com"><i class="ti-email"></i> <?= $contact['email'] ?></a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="col-lg-12 col-md-6">
+                        <h5>Head Office(Philippines):</h5>
+                        <ul class="contacts">
+                            <li><?= $contact['head_office_address'] ?></li>
+                            <li><a href="tel://+63 297 2612"><i class="ti-mobile"></i> <?= $contact['head_office_number'] ?></a>
+                            </li>
+                            <li><a href="mailto:info@juanderertravels.com"><i class="ti-email"></i> <?= $contact['email'] ?></a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
             </div>
-            <div class="col-lg-3 col-md-6">
-                <h5>Head Office(Philippines):</h5>
-                <ul class="contacts">
-                    <li><?= $contact['head_office_address'] ?></li>
-                    <li><a href="tel://+63 297 2612"><i class="ti-mobile"></i> <?= $contact['head_office_number'] ?></a>
-                    </li>
-                    <li><a href="mailto:info@juanderertravels.com"><i class="ti-email"></i> <?= $contact['email'] ?></a>
-                    </li>
-                </ul>
-            </div>
+
         </div>
         <!--/row-->
         <hr>
